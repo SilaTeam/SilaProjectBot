9181b3b5ef461d9d261b077ef4acc9198483f9f3
Shahab <safaeishahab@yahoo.com>
2016-04-28 13:06:28 +0430

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "Do you want me to install libreadline? (yes/no):"
+
+if [ "$REPLY" != "yes" ]; then
+	echo "
+"
+else
+	echo "SILA Team"
+        sudo apt-get update
+        sudo apt-get upgrade
+	sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev
+
+fi
